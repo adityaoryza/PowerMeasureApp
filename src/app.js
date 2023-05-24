ad453e20a24eec584006d0dad6373ea163bd46bc
adityaoryza <oryza429@gmail.com>
2023-05-24 21:15:07 +0700

    update windows compaibility

--- src/app.js
+++ src/app.js
@@ -1,60 +1,3 @@
-// const express = require("express");
-// const pidusage = require("pidusage");
-// const si = require("systeminformation");
-// const { exec } = require("child_process");
-
-// const app = express();
-// const port = 3030;
-
-// app.set("view engine", "ejs");
-
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
-
-// app.get("/usage", (req, res) => {
-//   const processName = req.query.processName;
-
-//   exec(`pgrep -x ${processName}`, async (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(
-//         `Error occurred while finding process ${processName}: ${error.message}`
-//       );
-//       res.send(`Error occurred while finding process ${processName}`);
-//       return;
-//     }
-//     if (stderr) {
-//       console.error(
-//         `Error occurred while finding process ${processName}: ${stderr}`
-//       );
-//       res.send(`Error occurred while finding process ${processName}`);
-//       return;
-//     }
-
-//     const pid = stdout.trim();
-//     if (!pid) {
-//       res.send(`No process named ${processName} found.`);
-//       return;
-//     }
-
-//     try {
-//       const [cpuStats, memStats] = await Promise.all([pidusage(pid), si.mem()]);
-
-//       const cpuUsage = cpuStats.cpu;
-//       const memUsage = (memStats.used / memStats.total) * 100;
-
-//       res.json({ cpuUsage, memUsage });
-//     } catch (error) {
-//       console.error("Error occurred while monitoring system usage:", error);
-//       res.send("Error occurred while monitoring system usage");
-//     }
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
 const express = require("express");
 const pidusage = require("pidusage");
 const si = require("systeminformation");
@@ -72,50 +15,91 @@ app.get("/", (req, res) => {
 app.get("/usage", (req, res) => {
   const processName = req.query.processName;
 
-  let pidCommand;
-  let pidArgument;
   if (process.platform === "win32") {
-    pidCommand = "tasklist";
-    pidArgument = `/FI "IMAGENAME eq ${processName}.exe"`;
+    // Windows-specific handling
+    exec(
+      `tasklist /FI "IMAGENAME eq ${processName}"`,
+      async (error, stdout, stderr) => {
+        if (error) {
+          console.error(
+            `Error occurred while finding process ${processName}: ${error.message}`
+          );
+          res.send(`Error occurred while finding process ${processName}`);
+          return;
+        }
+        if (stderr) {
+          console.error(
+            `Error occurred while finding process ${processName}: ${stderr}`
+          );
+          res.send(`Error occurred while finding process ${processName}`);
+          return;
+        }
+
+        const lines = stdout.trim().split("\r\n");
+        if (lines.length < 2) {
+          res.send(`No process named ${processName} found.`);
+          return;
+        }
+
+        const processInfo = lines[1].split(/\s+/);
+        const pid = processInfo[1];
+
+        try {
+          const [cpuStats, memStats] = await Promise.all([
+            pidusage(pid),
+            si.mem(),
+          ]);
+
+          const cpuUsage = cpuStats.cpu;
+          const memUsage = (memStats.used / memStats.total) * 100;
+
+          res.json({ cpuUsage, memUsage });
+        } catch (error) {
+          console.error("Error occurred while monitoring system usage:", error);
+          res.send("Error occurred while monitoring system usage");
+        }
+      }
+    );
   } else {
-    pidCommand = "pgrep";
-    pidArgument = `-x ${processName}`;
+    // Non-Windows handling (assuming Unix-like system)
+    exec(`pgrep -x ${processName}`, async (error, stdout, stderr) => {
+      if (error) {
+        console.error(
+          `Error occurred while finding process ${processName}: ${error.message}`
+        );
+        res.send(`Error occurred while finding process ${processName}`);
+        return;
+      }
+      if (stderr) {
+        console.error(
+          `Error occurred while finding process ${processName}: ${stderr}`
+        );
+        res.send(`Error occurred while finding process ${processName}`);
+        return;
+      }
+
+      const pid = stdout.trim();
+      if (!pid) {
+        res.send(`No process named ${processName} found.`);
+        return;
+      }
+
+      try {
+        const [cpuStats, memStats] = await Promise.all([
+          pidusage(pid),
+          si.mem(),
+        ]);
+
+        const cpuUsage = cpuStats.cpu;
+        const memUsage = (memStats.used / memStats.total) * 100;
+
+        res.json({ cpuUsage, memUsage });
+      } catch (error) {
+        console.error("Error occurred while monitoring system usage:", error);
+        res.send("Error occurred while monitoring system usage");
+      }
+    });
   }
-
-  exec(`${pidCommand} ${pidArgument}`, async (error, stdout, stderr) => {
-    if (error) {
-      console.error(
-        `Error occurred while finding process ${processName}: ${error.message}`
-      );
-      res.send(`Error occurred while finding process ${processName}`);
-      return;
-    }
-    if (stderr) {
-      console.error(
-        `Error occurred while finding process ${processName}: ${stderr}`
-      );
-      res.send(`Error occurred while finding process ${processName}`);
-      return;
-    }
-
-    const pid = stdout.trim();
-    if (!pid) {
-      res.send(`No process named ${processName} found.`);
-      return;
-    }
-
-    try {
-      const [cpuStats, memStats] = await Promise.all([pidusage(pid), si.mem()]);
-
-      const cpuUsage = cpuStats.cpu;
-      const memUsage = (memStats.used / memStats.total) * 100;
-
-      res.json({ cpuUsage, memUsage });
-    } catch (error) {
-      console.error("Error occurred while monitoring system usage:", error);
-      res.send("Error occurred while monitoring system usage");
-    }
-  });
 });
 
 app.listen(port, () => {
